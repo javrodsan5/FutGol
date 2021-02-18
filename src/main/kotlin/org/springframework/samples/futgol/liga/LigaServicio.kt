7fbd7fe49668fdb570ec3db60c900e471a7e6bab
Fernando <fercaldur@alum.us.es>
2021-02-18 14:47:58 +0100

    Actualizados métodos de web scraping con nuevos datos de jugadores.
    Se ha añadido las estadísticas de cada partido que haya realizado y la foto actualizada por Transfermarkt

--- src/main/kotlin/org/springframework/samples/futgol/liga/LigaServicio.kt
+++ src/main/kotlin/org/springframework/samples/futgol/liga/LigaServicio.kt
@@ -58,8 +58,7 @@ class LigaServicio {
         return res
     }
 
-    //de momento este será su sitio hasta que se cree el servicio de jugador
-    fun equiposYJugadores() {
+    fun equiposYEstadisticasJugadores() {
         val urlBase = "https://fbref.com"
         val doc = Jsoup.connect("$urlBase/es/comps/12/Estadisticas-de-La-Liga").get()
         val linksEquipos = doc.select("#results107311_overall:first-of-type tr td:first-of-type a")
@@ -68,94 +67,110 @@ class LigaServicio {
         var linksJug: MutableList<String> = ArrayList()
         for (linkEquipo in linksEquipos) {
             val doc2 = Jsoup.connect("$urlBase" + linkEquipo).get()
-            linksJug.addAll(doc2.select("table.min_width.sortable.stats_table#stats_standard_10731 th a:first-of-type")
-                .map { col -> col.attr("href") }.stream().distinct().collect(Collectors.toList())
+            //linkPartidosJugador= doc2.select("table.min_width.sortable.stats_table#stats_standard_10731 td.left.group_start a").attr("href"))
+            linksJug.addAll(
+                doc2.select("table.min_width.sortable.stats_table#stats_standard_10731 th a:first-of-type")
+                    .map { col -> col.attr("href") }.stream().distinct().collect(Collectors.toList())
             )
             linksJug = linksJug.stream().distinct().collect(Collectors.toList()) //todos los links jugadores de la liga
 
         }
-        for (n in 0 until linksJug.size - 1) {
-            val doc3 = Jsoup.connect("$urlBase" + linksJug[n]).get()
-            println(linksJug[n]) //link jugador
-            var foto: String
-            if (doc3.select("div.players#info div#meta img").map { col -> col.attr("src") }.size != 0) {
-                foto = doc3.select("div.players#info div#meta img").map { col -> col.attr("src") }[0] //foto jugador
-                println(foto)
-            } else {
-                foto = "No dispone de foto."
-            }
-            var element: Elements? = doc3.select("div.players#info div#meta p")
-            var posicion: String
-            var pie: String
-            var altura: Double
-            var peso: Double
-            var nacimiento: String
-            var club: String
-            for (n in 0 until element?.size!!) {
-                if (element[n].text().contains("Posición:")) {
-                    posicion = element[n].text().split("▪")[0].substringAfter(": ")
-                    println(posicion)
-                }
-                if (element[n].text().contains("Pie primario:")) {
-                    pie = if (element[n].text().contains("%")) {
-                        element[n].text().split("▪")[1].substringAfter("% ").replace("*", "") //pie primario
-
-                    } else {
-                        element[n].text().split("▪")[1].substringAfter(": ") //pie primario
+        for(linkJugador in linksJug){
+            println(linkJugador)
+            val doc3 = Jsoup.connect("$urlBase" + linkJugador).get()
+            if(!doc3.select("table#stats_standard_dom_lg.min_width.sortable.stats_table.shade_zero tbody tr").isEmpty()) {
+                var ultimaTemporada =
+                    doc3.select("table#stats_standard_dom_lg.min_width.sortable.stats_table.shade_zero tbody tr").last()
+                var nombreUltimaTemporada = (ultimaTemporada.select("th").first().text()) //tiene que ser 2020-2021
+                var ligaUltimaTemporada = ultimaTemporada.select("td")[3].text()
+                var partidosUltimaTemporada = ultimaTemporada.select("td").last().select("a").attr("href")
+                if (nombreUltimaTemporada.equals("2020-2021") && ligaUltimaTemporada.contains("La Liga")) { //h
+                    val doc4 = Jsoup.connect("$urlBase" + partidosUltimaTemporada).get()
+                    var filtroCompeticiones= doc4.select("div.filter").first().select("a")
+                    var filtro = ""
+                    for(n in 0 until filtroCompeticiones.size){
+                        if(filtroCompeticiones[n].text()=="2020-2021 La Liga"){
+                            filtro = filtroCompeticiones[n].attr("href")
+                        }
                     }
-                    println(pie)
+                    val doc5 = Jsoup.connect("$urlBase" + filtro).get()
+                    var partidos= doc5.select("table.min_width.sortable.stats_table.shade_zero tbody tr")
+
+                    var filtroRegistros= doc4.select("div.filter")[1].select("a")
+
+                    var filtro2=""
+                    for(n in 0 until filtroRegistros.size){
+                        if(filtroRegistros[n].text()=="Porteros"){
+                            filtro2 = filtroCompeticiones[n].attr("href")
+                        }
+                    }
+
+                    for(n in 0 until partidos.size){
+                        println("Equipo local: " + partidos[n].select("td[data-stat=squad]").text())
+                        println("Equipo visitante: " + partidos[n].select("td[data-stat=opponent]").text())
+                        println("Fecha del partido: " + partidos[n].select("th[data-stat=date] a").text())
+                        println("Jornada: " + partidos[n].select("td[data-stat=round]").text())
+                        println("Resultado: " + partidos[n].select("td[data-stat=result]").text())
+
+                        println("------- Mis estadísticas de ese partido -------")
+                        println("Minutos jugados: " + partidos[n].select("td[data-stat=minutes]").text())
+                        println("Goles: " + partidos[n].select("td[data-stat=goals]").text())
+                        println("Asistencias: " + partidos[n].select("td[data-stat=assists]").text())
+                        println("Penaltis marcados: " + partidos[n].select("td[data-stat=pens_made]").text())
+                        println("Penaltis lanzados: " + partidos[n].select("td[data-stat=pens_att]").text())
+                        println("Disparos totales: " + partidos[n].select("td[data-stat=shots_total]").text())
+                        println("Disparos a puerta: " + partidos[n].select("td[data-stat=shots_on_target]").text())
+                        println("Tarjetas amarillas: " + partidos[n].select("td[data-stat=cards_yellow]").text())
+                        println("Tarjetas rojas: " + partidos[n].select("td[data-stat=cards_red]").text())
+                        println("Robos: " + partidos[n].select("td[data-stat=interceptions]").text())
+                        println("Bloqueos: " + partidos[n].select("td[data-stat=blocks]").text())
+
+                        println("---------------------------------------------")
+                    }
 
                 }
-                if (element[n].text().contains("cm") && isNumeric(element[n].text()[0].toInt())) {
-                    altura = element[n].text().split(",")[0].substringBefore("cm").trim().toDouble()
-                    println(altura)
-
-                }
-                if (element[n].text().contains("kg") && isNumeric(element[n].text()[0].toInt())) {
-                    peso = element[n].text().split(",")[1].substringBefore("kg").trim().toDouble()
-                    println(peso)
-
-                }
-                if (element[n].text().contains("Nacimiento:")) {
-                    nacimiento = element[n].text().substringAfter("Nacimiento: ")
-                    println(nacimiento)
-
-                }
-                if (element[n].text().contains("Club")) {
-                    club = element[n].text().substringAfter("Club : ").trim()
-                    println(club)
-                }
+            }else{
+                println("Este jugador no tiene estadísticas de esta temporada.")
             }
         }
-
     }
 
-    fun precio() {// tambien coger estado del jugador: lesionado, en forma...
+    fun precioEstadoYFoto() {// tambien coger estado del jugador: lesionado, en forma...
         val urlBase = "https://www.transfermarkt.es"
         val doc = Jsoup.connect("$urlBase/laliga/startseite/wettbewerb/ES1/plus/?saison_id=2020").get()
-        val linksEquipos =
-            doc.select("div.box.tab-print td a.vereinprofil_tooltip").map { col -> col.attr("href") }.stream()
-                .distinct().collect(Collectors.toList())
-        for (linkEquipo in linksEquipos) {
-            val doc2 = Jsoup.connect("$urlBase" + linkEquipo).get()
-            val linkPlantilla =
-                doc2.select("li#vista-general.first-button a").map { col -> col.attr("href") }.stream().distinct()
-                    .collect(Collectors.toList())
-            val doc3 = Jsoup.connect("$urlBase" + linkPlantilla[0]).get()
-            val nombresJugadores = doc3.select("div.responsive-table tbody tr td div.di.nowrap:first-of-type span a")
-            val precioJugadores = doc3.select("div.responsive-table tbody tr td.rechts.hauptlink")
-            for (n in 0 until nombresJugadores.size) {
-                println((nombresJugadores[n].attr("title")))
-                var precio = (precioJugadores[n]).text()
-                var precioD = 0.0
-                if (precio.isNotEmpty()) {
-                    precioD = if (precio.contains("mill")) {
-                        precio.substringBefore(" mil").replace(",", ".").toDouble()
-                    } else {
-                        precio.substringBefore(" mil").replace(",", ".").toDouble() / 1000//pasar a millones
+        val linksEquipos= doc.select("div.box.tab-print td a.vereinprofil_tooltip").map { col -> col.attr("href") }.stream().distinct().collect(Collectors.toList())
+        for (linkEquipo in linksEquipos){
+            val doc2 = Jsoup.connect("$urlBase"+linkEquipo).get()
+            val linkPlantilla= doc2.select("li#vista-general.first-button a").map { col -> col.attr("href") }.stream().distinct().collect(Collectors.toList())
+            val doc3 = Jsoup.connect("$urlBase"+linkPlantilla[0]).get()
+            val precioJugadores= doc3.select("div.responsive-table tbody tr td.rechts.hauptlink")
+            val jugadores= doc3.select("div#yw1.grid-view table.items tbody tr:first-of-type")
+            jugadores.removeAt(0)
+            for(n in 0 until jugadores.size){
+                var persona= jugadores[n].select("td div.di.nowrap:first-of-type span a")
+                var nombre = persona.attr("title")
+                var linkDetallePersona = persona.attr("href")
+                val doc4 = Jsoup.connect("$urlBase"+linkDetallePersona).get()
+                var foto= doc4.select("div.dataBild img").attr("src")
+                var estado= "En forma"
+                if(!jugadores[n].select("td span.verletzt-table").isEmpty()){
+                    estado= "Lesionado"
+                }else if(!jugadores[n].select("td span.ausfall-6-table").isEmpty()){
+                    estado= "Sancionado/No disponible"
+                }
+                var precio= precioJugadores[n].text()
+                var precioD =0.1
+                if(!precio.isEmpty()) {
+                    if(precio.contains("mill")){
+                        precioD = precio.substringBefore(" mil").replace(",", ".").toDouble()
+                    }else{
+                        precioD = precio.substringBefore(" mil").replace(",", ".").toDouble()/1000//pasar a millones
                     }
                 }
+                println(nombre)
                 println(precioD)
+                println(estado)
+                println(foto)
 
             }
         }

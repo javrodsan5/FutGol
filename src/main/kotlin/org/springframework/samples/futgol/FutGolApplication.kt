2c15294496c809f73b63ded2ce4c4f743b58c507
Fernando <fercaldur@alum.us.es>
2021-02-19 12:33:59 +0100

    Añadido método WebScraping de datos clasificación de equipos de laLiga.

--- src/main/kotlin/org/springframework/samples/futgol/FutGolApplication.kt
+++ src/main/kotlin/org/springframework/samples/futgol/FutGolApplication.kt
@@ -1,6 +1,5 @@
 package org.springframework.samples.futgol
 
-import org.jsoup.Jsoup
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.boot.jdbc.DataSourceBuilder

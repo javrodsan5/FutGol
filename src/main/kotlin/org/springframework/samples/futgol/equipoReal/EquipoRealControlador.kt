701674788de8b949e5d3e65496b4396ff0c066ff
javrodsan5 <javrodsan5@alum.us.es>
2021-03-02 21:19:23 +0100

    Cambio vista equipo real y pruebas unitarias de partido
    Puesta la vista del equipo real con el campo de fútbol y realizadas las pruebas unitarias de partido

--- src/main/kotlin/org/springframework/samples/futgol/equipoReal/EquipoRealControlador.kt
+++ src/main/kotlin/org/springframework/samples/futgol/equipoReal/EquipoRealControlador.kt
@@ -6,6 +6,7 @@ import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import java.util.stream.Collectors
 
 @Controller
 class EquipoRealControlador(val equipoRealServicio: EquipoRealServicio) {
@@ -27,7 +28,7 @@ class EquipoRealControlador(val equipoRealServicio: EquipoRealServicio) {
 
     @GetMapping("/equiposLiga")
     fun listaEquiposReales(model: Model): String {
-        model["equiposReales"] = equipoRealServicio.buscarTodosEquiposReales()!!.sortedBy { x-> x.posicion }
+        model["equiposReales"] = equipoRealServicio.buscarTodosEquiposReales()!!.sortedBy { x -> x.posicion }
         return VISTA_LISTA_EQUIPOSREALES
     }
 
@@ -36,13 +37,30 @@ class EquipoRealControlador(val equipoRealServicio: EquipoRealServicio) {
         var equipo = equipoRealServicio.buscarEquipoRealPorNombre(nombreEquipo)!!
         model["equipo"] = equipo
         var jugadores = equipo.jugadores
-        model["partidosLocal"] = equipo.partidosLocal
-        model["partidosVisitante"] = equipo.partidosVisitante
+        var portero = equipo.jugadores.stream()?.filter { x -> x?.posicion == "PO" }.filter{x->x.estadoLesion=="En forma"}
+            .sorted(Comparator.comparing { x -> -x.valor })?.findFirst()?.get()
+        model["portero"] = portero!!
+        var defensas = equipo.jugadores.stream()?.filter { x -> x?.posicion == "DF" }.filter{x->x.estadoLesion=="En forma"}
+            .sorted(Comparator.comparing { x -> -x.valor })
+            ?.limit(4)
+            ?.collect(Collectors.toList())
+        model["defensas"] = defensas!!
+        var centrocampistas = equipo.jugadores.stream()?.filter { x -> x?.posicion == "CC" }.filter{x->x.estadoLesion=="En forma"}
+            .sorted(Comparator.comparing { x -> -x.valor })
+            ?.limit(4)
+            ?.collect(Collectors.toList())
+        model["centrocampistas"] = centrocampistas!!
+        var delanteros = equipo.jugadores.stream()?.filter { x -> x?.posicion == "DL" }.filter{x->x.estadoLesion=="En forma"}
+            .sorted(Comparator.comparing { x -> -x.valor })
+            ?.limit(2)
+            ?.collect(Collectors.toList())
+        model["delanteros"] = delanteros!!
+        model["partidosLocal"] = equipo.partidosLocal.sortedBy { x -> x.jornada?.numeroJornada }
+        model["partidosVisitante"] = equipo.partidosVisitante.sortedBy { x -> x.jornada?.numeroJornada }
         model["jugadores"] = jugadores
         if (jugadores.size > 5) {
             model["top5Jugadores"] = equipo.jugadores.sortedBy { x -> -x.puntos }.subList(0, 5)
         }
         return VISTA_DETALLES_EQUIPOREAL
     }
-
 }

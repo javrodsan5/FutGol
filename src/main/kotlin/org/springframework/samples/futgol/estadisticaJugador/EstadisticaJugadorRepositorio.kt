a79c374ef5d94da248ed5a9f4a0a9d6ada87f6d4
javrodsan5 <javrodsan5@alum.us.es>
2021-03-09 12:59:03 +0100

    Detalles jugador con jornadas
    Implementados los detalles de un jugador pudiendo buscar sus estadísticas por jornada

--- src/main/kotlin/org/springframework/samples/futgol/estadisticaJugador/EstadisticaJugadorRepositorio.kt
+++ src/main/kotlin/org/springframework/samples/futgol/estadisticaJugador/EstadisticaJugadorRepositorio.kt
@@ -18,4 +18,7 @@ interface EstadisticaJugadorRepositorio : Repository<EstadisticaJugador, Int> {
     @Query("SELECT e FROM EstadisticaJugador e WHERE e.partido.id= ?1")
     fun buscarEstadisticasPorPartido(idPartido: Int): Collection<EstadisticaJugador>
 
+    @Query("SELECT e FROM EstadisticaJugador e where e.jugador.id = ?1 AND e.partido.jornada.numeroJornada = ?2")
+    fun buscarEstadisticasPorJugadorJornada(idJugador: Int, numeroJornada: Int?): EstadisticaJugador
+
 }

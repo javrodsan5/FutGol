db5dc73a85439ac0ff9ed41065041b8ea03af97e
Fernando <fercaldur@alum.us.es>
2021-03-21 18:00:09 +0100

    Incluida encriptación BCryptPasswordEncoder.
    Se ha añadido BCryptPasswordEncoder tanto para guardar como para iniciar sesión con contraseñas encriptadas con este algoritmo de cifrado.

---
+++ src/main/kotlin/org/springframework/samples/futgol/login/UserLoginServicio.kt
@@ -0,0 +1,25 @@
+package org.springframework.samples.futgol.login
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class UserLoginServicio : UserDetailsService {
+
+    @Autowired
+    private val userService: UserServicio? = null
+
+    override fun loadUserByUsername(username: String?): UserDetails {
+        var user: User? =
+            userService?.findUser(username) ?: throw UsernameNotFoundException("Usuario " + username + "no encontrado.")
+        var grantedAuthorities = ArrayList<GrantedAuthority>()
+        grantedAuthorities.add(SimpleGrantedAuthority("usuario"))
+        return org.springframework.security.core.userdetails.User(user?.username, user?.password, grantedAuthorities)
+    }
+
+}

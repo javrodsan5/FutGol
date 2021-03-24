6a7709a7c1bc4a215476857ebb386f73534b2c84
javrodsan5 <javrodsan5@alum.us.es>
2021-03-24 18:26:22 +0100

    Intento arreglo login heroku

--- src/main/kotlin/org/springframework/samples/futgol/system/LoginController.kt
+++ src/main/kotlin/org/springframework/samples/futgol/system/LoginController.kt
@@ -2,17 +2,20 @@ package org.springframework.samples.futgol.system
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class LoginController {
 
-    @RequestMapping("/login")
-    fun login(): String = "/login"
+    @GetMapping("/login")
+    fun login(): String {
+        return "/login"
+    }
 
-    @RequestMapping("/loginError")
-    fun loginError(model: Model): String{
-        model.addAttribute("loginError",true)
+    @GetMapping("/loginError")
+    fun loginError(model: Model): String {
+        model.addAttribute("loginError", true)
         return "/login"
     }
 }

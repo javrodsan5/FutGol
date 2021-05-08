107c224af2ddecf1948626d62c7212ca8604a6b9
Fernando <fercaldur@alum.us.es>
2021-05-08 13:14:29 +0200

    Mejora general visual de la web.

--- src/main/kotlin/org/springframework/samples/futgol/puja/PujaRepositorio.kt
+++ src/main/kotlin/org/springframework/samples/futgol/puja/PujaRepositorio.kt
@@ -21,4 +21,7 @@ interface PujaRepositorio : Repository<Puja, Int> {
 
     @Query("SELECT p FROM Puja p where p.jugador.id = ?1 AND p.subasta.id = ?2")
     fun buscarPujasJugadorSubasta(idJugador: Int,  idSubasta: Int): Collection<Puja>
+
+    @Query("SELECT p FROM Puja p WHERE p.equipo.id = ?1 AND p.jugador.id = ?2 AND p.subasta.id = ?3")
+    fun buscarPujaPorEquipoJugadorSubasta(idEquipo: Int, idJugador: Int, idSubasta: Int): Puja
 }
